667549b4a1bb9ed579eaecd2c7faa41673f4420e
SergioArmando <79291975+SergioArmando@users.noreply.github.com>
2021-05-14 23:36:28 -0600

    Add files via upload

---
+++ conecxio/Clases/BaseDatos/ClsConecxion.cs
@@ -0,0 +1,105 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace serg.Clases.BaseDatos
+{
+    public class ClsConecxion
+    {
+        public MySqlConnection conexion;
+        private String _conexion { get; }
+
+        public ClsConecxion()
+        {
+
+            _conexion = "Server=localhost;Database=alumnos; Port=3306; Username=root; Password=SoyAgente2341;";
+
+        }
+
+
+
+        /// <summary>
+        ///cerrar
+        /// </summary>
+        public void cerrarConexionBD()
+        {
+            conexion.Close();
+        }
+
+
+
+        /// <summary>
+        /// abrir
+        /// </summary>
+        public void abrirConexion()
+        {
+            conexion = new MySqlConnection(_conexion);
+            conexion.Open();
+        }
+
+
+
+
+        /// <summary>
+        ///  clausura a la base de datos
+        /// </summary>
+        /// <param name="sqll"></param>
+        /// <returns></returns>
+        public DataTable consultaTablaDirecta(String sqll)
+        {
+            abrirConexion();
+            MySqlDataReader dr;
+            MySqlCommand comm = new MySqlCommand(sqll, conexion);
+            dr = comm.ExecuteReader();
+
+            var dataTable = new DataTable();
+            dataTable.Load(dr);
+            cerrarConexionBD();
+            return dataTable;
+        }
+
+
+
+        /// <summary>
+        /// ejecuta una instrucción de insersion, eliminación y actualización,
+        /// 
+        /// </summary>
+        /// <param name="sqll"></param>
+        public void EjecutaSQLDirecto(String sqll)
+        {
+            abrirConexion();
+            try
+            {
+
+                MySqlCommand comm = new MySqlCommand(sqll, conexion);
+                comm.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+            finally
+            {
+                cerrarConexionBD();
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// ejecuta instrucciones SQL
+        /// 
+        /// </summary>
+        /// <param name="sqll"></param>
+        public void EjecutaSQLManual(String sqll)
+        {
+            // se abre y cierra la conecxion
+            MySqlCommand comm = new MySqlCommand(sqll, conexion);
+            comm.ExecuteReader();
+        }
+    }
+}
---
+++ conecxio/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using P1Acrud.Clases.Archivos;
+using P1Acrud.Clases.BaseDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace P1Acrud
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CargarArchivoExterno()
+        {
+            string Fuente = ((@"C:\Users\anner\Desktop\crudDB.csv"));
+            ClsArchivos ar = new ClsArchivos();
+            //obtener todo el archivo linea por linea dentro de un arreglo simple--unidimensional
+            string[] ArregloNotas = ar.LeerArchivo(Fuente);
+            string sentencia_sql = "";
+            int NumeroLinea = 0;
+
+            ClsConecxion cn = new ClsConecxion();
+            foreach (string linea in ArregloNotas)
+            {
+                //obteniendo datos
+                string[] datos = linea.Split(';');
+                if(NumeroLinea > 0)
+                {
+                    sentencia_sql += $"INSERT INTO tb_alumnos values(" + datos[0] + ", '" + datos[1] + "', " + datos[2] + ", " + datos[3] + ", " + datos[4] + ", " + datos[5] + ", '" + datos[6] + "'\n);";
+                }
+                NumeroLinea++;
+            }
+            cn.EjecutaSQLDirecto(sentencia_sql);
+        }
+
+
+        protected void SubirInformacion_Click(object sender, EventArgs e)
+        {
+                CargarArchivoExterno();
+        }
+    }
+}
